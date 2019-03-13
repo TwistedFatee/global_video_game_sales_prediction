ab7095c99ae9d2bf65116530c50f29b14d3817ef
cpatsel <therixy@gmail.com>
2019-03-13 10:51:08 -0400

    preprocessing code and cleaned csv

--- preprocess.r
+++ preprocess.r
@@ -1,6 +1,14 @@
-# Clean, and discretize dataset
+### This script will clean, and discretize dataset
+if (!require("rstudioapi")){
+  install.packages("rstudioapi")
+  library("rstudioapi")
+}
+#Set Working directory
+current_path <- getActiveDocumentContext()$path 
+setwd(dirname(current_path ))
 
-vgdata <- read.csv("data/vg.csv")
+
+vgdata <- read.csv("data/vg.csv", na.strings=c("","NA"),stringsAsFactors = FALSE)
 
 #Remove huge outliers, specifically, Wii Sports at 80m sales.
 no.outliers <- vgdata$Global_Sales[vgdata$Global_Sales < 20]
@@ -19,16 +27,18 @@ high.sales <- which(vgdata$Global_Sales > meanrange.upper)
 ###Clean up missing values
 
 #Missing Values in Critic_Score
-#Take the mean, disregarding NA values.
-critic.mean <- mean(vgdata$Critic_Score, na.rm = TRUE)
-critic.nalocs <- which(is.na(vgdata$Critic_Score))
+  #Take the mean, disregarding NA values.
+  critic.mean <- mean(vgdata$Critic_Score, na.rm = TRUE)
+  critic.nalocs <- which(is.na(vgdata$Critic_Score))
 
-#TODO: do this for the other categories.
+#Missing values in User_Score
+    user.mean <- mean(as.numeric(vgdata$User_Score), na.rm = TRUE)
+    user.nalocs <- which(is.na(as.numeric(vgdata$User_Score)))
 
 #### Create new dataset containing our preprocessed data.
 
 cnames <- c("Name","Sales","CriticalScore","UserScore","Genre","Developer")
-gameclass <- data.frame(matrix(NA, nrowt = 16719, ncol = 6))
+gameclass <- data.frame(matrix(NA, nrow = 16719, ncol = 6))
 colnames(gameclass) <- cnames
 
 #Transfer existing columns
@@ -38,14 +48,55 @@ gameclass$Developer = vgdata$Developer
 gameclass$CriticalScore = vgdata$Critic_Score
 gameclass$UserScore = vgdata$User_Score
 
-#Assign high/med/low classification.
+#Fill in missing data
+genre_default <- "Other"
+dev_default <- "Other"
+
+
+gameclass$CriticalScore[critic.nalocs] = critic.mean
+gameclass$UserScore[user.nalocs] = user.mean
+gameclass$Sales[which(is.na(gameclass$Sales))] = "Medium"
+gameclass$Genre[which(is.na(gameclass$Genre))] = genre_default
+gameclass$Developer[which(is.na(gameclass$Developer))] = dev_default
+
+
+
+
+
+##Assign high/med/low classification for sales.
 gameclass$Sales[high.sales] = "High"
 gameclass$Sales[med.sales] = "Medium"
 gameclass$Sales[low.sales] = "Low"
 
-#Fill in missing data
-gameclass$CriticalScore[critic.nalocs] = critic.mean
-gameclass$Sales[which(is.na(gameclass$Sales))] = "Medium"
+##Discretize the scores
+
+#Range for low/med/hi +/- 10% of average
+score_avg_upper <- critic.mean*1.1
+score_avg_lower <- critic.mean*0.9
+
+#Critic scores
 
 
+low_scores <- which(gameclass$CriticalScore < score_avg_lower)
+high_scores <- which(gameclass$CriticalScore > score_avg_upper)
+med_scores <- which(gameclass$CriticalScore >= score_avg_lower & gameclass$CriticalScore <= score_avg_upper)
+gameclass$CriticalScore[high_scores] = "High"
+gameclass$CriticalScore[med_scores] = "Medium"
+gameclass$CriticalScore[low_scores] = "Low"
+
+# User Scores
+
+score_avg_upper <- critic.mean*1.1
+score_avg_lower <- critic.mean*0.9
+
+#Critic scores
+low_scores <- which(gameclass$UserScore < score_avg_lower)
+high_scores <- which(gameclass$UserScore > score_avg_upper)
+med_scores <- which(gameclass$UserScore >= score_avg_lower & gameclass$UserScore <= score_avg_upper)
+gameclass$UserScore[high_scores] = "High"
+gameclass$UserScore[med_scores] = "Medium"
+gameclass$UserScore[low_scores] = "Low"
+
+
+write.csv(gameclass,"./data/cleaned.csv")
 
--- vgeda.r
+++ vgeda.r
@@ -34,3 +34,55 @@ Boston.rf=randomForest(medv ~ . , data = Boston , subset = train)
 Boston.rf
 plot(Boston.rf)
 
+
+eufit<- lm(vgdata$Global_Sales~vgdata$EU_Sales)
+plot(vgdata$Global_Sales~vgdata$EU_Sales)
+abline(eufit, col="red")
+
+
+jpfit<- lm(vgdata$Global_Sales~vgdata$JP_Sales)
+plot(vgdata$Global_Sales~vgdata$JP_Sales,log="x")
+abline(jpfit, col="red")
+boxplot(vgdata$JP_Sales)
+
+minmax_norm <- function(data, minval, maxval){
+ return ((data-min(data))/(max(data)-min(data)))*((maxval-minval)+minval)
+}
+
+znorm <- function (data){
+    return (data - mean(data))/sd(data)
+}
+
+gs <- minmax_norm(vgdata$Global_Sales,0,1)
+ns <- minmax_norm(vgdata$NA_Sales,0,1)
+
+glob <- znorm(vgdata$Global_Sales)
+north <- znorm(vgdata$NA_Sales)
+plot(glob~north)
+temp <- vgdata$Global_Sales * 1000000
+
+
+library(e1071)   
+skewness(gs)
+plot(gs[gs <= 0.05])
+
+nn <- minmax_norm(temp,0,1)
+skewness(nn)
+plot(density(nn))
+plot(density(log(nn)))
+plot(density(log(vgdata$Global_Sales)))
+plot(density(log(vgdata$Global_Sales[vgdata$Global_Sales <= 20])))
+lnn <- log(nn)
+
+plot(density(scale(gs)))
+#/*
+#minval <- 0
+#maxval <- 1
+#minmaxGS <- (vgdata$Global_Sales - max(vgdata$Global_Sales))/(max(vgdata$Global_Sales)-min(vgdata$Global_Sales))*
+#            (maxval-minval)+minval
+#
+plot (gs~ns)
+
+minmax_norm(vgdata)
+
+
